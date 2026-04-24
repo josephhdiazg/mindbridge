d06e443f6e8d46049160884d72db8752e54fe161
Joseph Diaz <194326262+josephhdiazg@users.noreply.github.com>
2026-04-24 13:58:31 -0500

    feat: add login and register requests

---
+++ src/main/java/com/usta/mindbridge/dto/request/RegisterRequest.java
@@ -0,0 +1,22 @@
+package com.usta.mindbridge.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RegisterRequest {
+
+    @NotBlank(message = "Name must not be blank")
+    private String name;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Email must be valid")
+    private String email;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+}
